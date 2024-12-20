06d7b3f1b8b38a1488f92c4f76c1c9ca54039d0e
Andrey <andreymaster100@gmail.com>
2024-12-20 20:47:57 +0300

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,21 @@
+
+number_of_tasks = 12
+hours_spent = 1.5
+
+course_name = 'Python'
+average_time_per_task = hours_spent / number_of_tasks
+print('Курс:', course_name,
+      ', всего задач:', number_of_tasks,
+      ', затрачено часов:', hours_spent,
+      ', среднее время выполнения', average_time_per_task, 'часа.')
+
+first_name ='Vasya'
+a='Vasya'
+apple_count = 10
+second_name = 'Petya'
+b='Petya'
+orange_count =15
+
+
+print(a,'дал',b,apple_count,'яблок и',orange_count,'апельсинов')
+
